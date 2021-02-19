1f2f94e029ace9773c8249c23049b6b0652ed643
Sam <samuel.prouten@ensta-bretagne.org>
2021-02-19 15:29:49 +0100

    Finished all lessons and switched to CLion

--- C++/Intro/Lesson_F/main.cpp
+++ C++/Intro/Lesson_F/main.cpp
@@ -5,38 +5,112 @@ using namespace tubex;
 
 bool intro = false;
 
-int main()
-{
-  if(intro)
-  {
+int main() {
+    if (intro) {
+        //--------------- Init
+        // trajectory of the robot
+        double dt = 0.01;
+        Interval tdomain(0, 5); // temporal domain [t0,tf]
+        TrajectoryVector actual_x(tdomain, TFunction("(2*cos(t) ; sin(2*t))"), dt);
+
+        //Speed
+        TrajectoryVector actual_v(tdomain, TFunction("(-2*sin(t) ; 2*cos(2*t))"), dt);
+
+        //Landmark
+        Vector b({0.5, 1});
+
+        //Measurement
+        IntervalVector yi({{-0.84, -0.83},
+                           {-0.76, -0.75}});
+
+        //---------------- Tubes
+        TubeVector x(tdomain, dt, 2);
+
+
+        TubeVector v = TubeVector(actual_v, dt);
+        v.inflate(0.03);
+
+        //-------------- Contraction
+        ContractorNetwork cn;
+        double ti = 2;
+        cn.add(ctc::deriv, {x, v});
+        cn.add(ctc::eval, {ti, yi, x, v});
+
+
+        //----------------- Graphics
+        vibes::beginDrawing();
+
+        VIBesFigMap fig_map("Map");
+        fig_map.set_properties(100, 100, 600, 300);
+        fig_map.add_trajectory(&actual_x, "x*", 0, 1);
+        fig_map.add_tube(&x, "x", 0, 1);
+        fig_map.add_beacon(b, 0.1);                   // 0.1: landmark width
+        fig_map.axis_limits(-2.5, 2.5, -0.1, 0.1, true);
+
+        cn.contract();
+
+        fig_map.show(0.5);
+    }
+
     //--------------- Init
-    // trajectory of the robot
+    // Actual trajectory of the state vector
     double dt = 0.01;
-    Interval tdomain(0,5); // temporal domain [t0,tf]
-    TrajectoryVector actual_x(tdomain, TFunction("(2*cos(t) ; sin(2*t))"), dt);
-    
-    //Speed
-    TrajectoryVector actual_v(tdomain, TFunction("(-2*sin(t) ; 2*cos(2*t))"), dt);
+    Interval tdomain(0, 3); // temporal domain [t0,tf]
+    TrajectoryVector actual_x(tdomain, TFunction(
+            "(10*cos(t) + t ; 5*sin(2*t) + t ; atan2((10*cos(2*t) + 1), -10*sin(t) + 1) ; sqrt((-10*sin(t) + 1)^2 + (10*cos(2*t) + 1)^2))"),
+                              dt);
+    IntervalVector u({{-oo, oo},
+                      {-oo, oo}});
 
-    //Landmark
-    Vector b({0.5, 1});
+    //Landmarks
+    IntervalVector b1({{8},
+                       {3}});
+    IntervalVector b2({{0},
+                       {5}});
+    IntervalVector b3({{-2},
+                       {1}});
 
-    //Measurement
-    IntervalVector yi({{-0.84, -0.83}, {-0.76, -0.75}});
+    // Measurements of psi and v
+    Tube psi = Tube(actual_x[2], dt);
+    psi.inflate(0.01);
+
+    Tube v = Tube(actual_x[3], dt);
+    psi.inflate(0.01);
+
+    // Measurements of distance to landmarks at certain times
+    double t1 = 0.3;
+    Interval y1(1.9);
+    y1.inflate(0.1);
+
+    double t2 = 1.5;
+    Interval y2(3.6);
+    y2.inflate(0.1);
+
+    double t3 = 2.0;
+    Interval y3(2.8);
+    y3.inflate(0.1);
 
     //---------------- Tubes
-    TubeVector x(tdomain, dt, 2);
-    
-
-    TubeVector v = TubeVector(actual_v, dt);
-    v.inflate(0.03);
+    TubeVector x(tdomain, dt, 4);
+    x[2] = psi;
+    x[3] = v;
 
     //-------------- Contraction
     ContractorNetwork cn;
-    double ti = 2;
-    cn.add(ctc::deriv, {x, v});
-    cn.add(ctc::eval, {ti, yi, x, v});
-
+    TubeVector a(tdomain, dt, 4);
+    IntervalVector p1(4);
+    IntervalVector p2(4);
+    IntervalVector p3(4);
+    CtcFunction f(Function("x[4]", "u[2]", "a[4]",
+                           "(x[3]*cos(x[2]) - a[0] ; x[3]*sin(x[2]) - a[1] ; u[0] - a[2] ; u[1] - a[3])"));
+    cn.add(ctc::deriv, {x, a});
+    cn.add(f, {x, u, a});
+    cn.add(ctc::dist, {p1[0], p1[1], b1, y1});
+    cn.add(ctc::dist, {p2[0], p2[1], b2, y2});
+    cn.add(ctc::dist, {p3[0], p3[1], b3, y3});
+    cn.add(ctc::eval, {t1, p1, x, a});
+    cn.add(ctc::eval, {t2, p2, x, a});
+    cn.add(ctc::eval, {t3, p3, x, a});
 
     //----------------- Graphics
     vibes::beginDrawing();
@@ -45,83 +119,15 @@ int main()
     fig_map.set_properties(100, 100, 600, 300);
     fig_map.add_trajectory(&actual_x, "x*", 0, 1);
     fig_map.add_tube(&x, "x", 0, 1);
-    fig_map.add_beacon(b, 0.1);                   // 0.1: landmark width
-    fig_map.axis_limits(-2.5,2.5,-0.1,0.1, true);
+    fig_map.add_beacon(b1, 0.1);
+    fig_map.add_beacon(b2, 0.1);
+    fig_map.add_beacon(b3, 0.1);
+    fig_map.draw_ring(b1[0].mid(), b1[1].mid(), y1, "gray");
+    fig_map.draw_ring(b2[0].mid(), b2[1].mid(), y2, "gray");
+    fig_map.draw_ring(b3[0].mid(), b3[1].mid(), y3, "gray");
+    fig_map.axis_limits(-2.5, 2.5, -0.1, 0.1, true);
 
     cn.contract();
 
     fig_map.show(0.5);
-  }
-
-  //--------------- Init
-  // Actual trajectory of the state vector
-  double dt = 0.01;
-  Interval tdomain(0,3); // temporal domain [t0,tf]
-  TrajectoryVector actual_x(tdomain, TFunction("(10*cos(t) + t ; 5*sin(2*t) + t ; atan2((10*cos(2*t) + 1), -10*sin(t) + 1) ; sqrt((-10*sin(t) + 1)^2 + (10*cos(2*t) + 1)^2))"), dt);
-  IntervalVector u({{-oo, oo}, {-oo, oo}});
-
-  //Landmarks
-  IntervalVector b1({{8}, {3}});
-  IntervalVector b2({{0}, {5}});
-  IntervalVector b3({{-2}, {1}});
-
-  // Measurements of psi and v
-  Tube psi = Tube(actual_x[2], dt);
-  psi.inflate(0.01);
-
-  Tube v = Tube(actual_x[3], dt);
-  psi.inflate(0.01);
-
-  // Measurements of distance to landmarks at certain times
-  double t1 = 0.3;
-  Interval y1(1.9);
-  y1.inflate(0.1);
-  
-  double t2 = 1.5;
-  Interval y2(3.6);
-  y2.inflate(0.1);
-  
-  double t3 = 2.0;
-  Interval y3(2.8);
-  y3.inflate(0.1);
-
-  //---------------- Tubes
-  TubeVector x(tdomain, dt, 4);
-  x[2] = psi;
-  x[3] = v;
-
-  //-------------- Contraction
-  ContractorNetwork cn;
-  TubeVector a(tdomain, dt, 4);
-  IntervalVector p1(4);
-  IntervalVector p2(4);
-  IntervalVector p3(4);
-  CtcFunction f(Function("x[4]", "u[2]", "a[4]", "(x[3]*cos(x[2]) - a[0] ; x[3]*sin(x[2]) - a[1] ; u[0] - a[2] ; u[1] - a[3])"));
-  cn.add(ctc::deriv, {x, a});
-  cn.add(f, {x, u, a});
-  cn.add(ctc::dist, {p1[0], p1[1], b1, y1});
-  cn.add(ctc::dist, {p2[0], p2[1], b2, y2});
-  cn.add(ctc::dist, {p3[0], p3[1], b3, y3});
-  cn.add(ctc::eval, {t1, p1, x, a});
-  cn.add(ctc::eval, {t2, p2, x, a});
-  cn.add(ctc::eval, {t3, p3, x, a});
-
-  //----------------- Graphics
-  vibes::beginDrawing();
-
-  VIBesFigMap fig_map("Map");
-  fig_map.set_properties(100, 100, 600, 300);
-  fig_map.add_trajectory(&actual_x, "x*", 0, 1);
-  fig_map.add_tube(&x, "x", 0, 1);
-  fig_map.add_beacon(b1, 0.1);
-  fig_map.add_beacon(b2, 0.1);
-  fig_map.add_beacon(b3, 0.1);
-  fig_map.draw_ring(b1[0].mid(), b1[1].mid(), y1, "gray");
-  fig_map.draw_ring(b2[0].mid(), b2[1].mid(), y2, "gray");
-  fig_map.draw_ring(b3[0].mid(), b3[1].mid(), y3, "gray");
-  fig_map.axis_limits(-2.5,2.5,-0.1,0.1, true);
-
-  cn.contract();
-
-  fig_map.show(0.5);
 }
--- C++/Intro/Lesson_G/main.cpp
+++ C++/Intro/Lesson_G/main.cpp
@@ -5,82 +5,85 @@ using namespace tubex;
 
 bool intro = false;
 
-int main()
-{
-  //--------------- Init
-  // Actual trajectory of the state vector
-  double dt = 0.05;
-  Interval tdomain(0,6); // temporal domain [t0,tf]
-  TrajectoryVector actual_x(tdomain, TFunction("(10*cos(t) + t ; 5*sin(2*t) + t ; atan2((10*cos(2*t) + 1), -10*sin(t) + 1) ; sqrt((-10*sin(t) + 1)^2 + (10*cos(2*t) + 1)^2))"), dt);
-  IntervalVector u({{-oo, oo}, {-oo, oo}});
+int main() {
+    //--------------- Init
+    // Actual trajectory of the state vector
+    double dt = 0.05;
+    Interval tdomain(0, 6); // temporal domain [t0,tf]
+    TrajectoryVector actual_x(tdomain, TFunction(
+            "(10*cos(t) + t ; 5*sin(2*t) + t ; atan2((10*cos(2*t) + 1), -10*sin(t) + 1) ; sqrt((-10*sin(t) + 1)^2 + (10*cos(2*t) + 1)^2))"),
+                              dt);
+    IntervalVector u({{-oo, oo},
+                      {-oo, oo}});
 
-  // Creating random map of landmarks
-  int nb_landmarks = 150;
-  IntervalVector map_area(actual_x.codomain().subvector(0,1));
-  map_area.inflate(2);
-  vector<IntervalVector> v_map =
-    DataLoader::generate_landmarks_boxes(map_area, nb_landmarks);
+    // Creating random map of landmarks
+    int nb_landmarks = 150;
+    IntervalVector map_area(actual_x.codomain().subvector(0, 1));
+    map_area.inflate(2);
+    vector<IntervalVector> v_map =
+            DataLoader::generate_landmarks_boxes(map_area, nb_landmarks);
 
-  // Generating observations obs=(t,range,bearing) of these landmarks
-  int max_nb_obs = 20;
-  Interval visi_range(0,4); // [0m,75m]
-  Interval visi_angle(-M_PI/4,M_PI/4); // frontal sonar
-  vector<IntervalVector> v_obs =
-    DataLoader::generate_observations(actual_x, v_map, max_nb_obs,
-                                      true, visi_range, visi_angle);
+    // Generating observations obs=(t,range,bearing) of these landmarks
+    int max_nb_obs = 20;
+    Interval visi_range(0, 4); // [0m,75m]
+    Interval visi_angle(-M_PI / 4, M_PI / 4); // frontal sonar
+    vector<IntervalVector> v_obs =
+            DataLoader::generate_observations(actual_x, v_map, max_nb_obs,
+                                              true, visi_range, visi_angle);
 
-  for (auto& obs : v_obs)
-  {
-    obs[1].inflate(0.1);
-    obs[2].inflate(0.04);
-  }
+    for (auto &obs : v_obs) {
+        obs[1].inflate(0.1);
+        obs[2].inflate(0.04);
+    }
 
-  //Tube
-  TubeVector x(tdomain, dt, 4);
-  x[2] = Tube(actual_x[2], dt).inflate(0.01);
-  x[3] = Tube(actual_x[3], dt).inflate(0.01);
-  
-  //-------------- Contraction
-  ContractorNetwork cn;
-  TubeVector v(tdomain, dt, 4); // Derivative of x
-  CtcFunction ctc_add(Function("x1", "x2", "x3", "x1 + x2 - x3")); //Add contractor function
-  CtcFunction ctc_sub(Function("x", "y", "z", "x - y - z")); //Sub contractor function
-  CtcConstell ctc_constell(v_map); //Constellation contractor on map v_map
-  for (auto& obs : v_obs) // For each observation
-  {
-    // State vector at the time of the observation
-    IntervalVector& p = cn.create_dom({Interval::ALL_REALS, Interval::ALL_REALS, Interval::ALL_REALS, Interval::ALL_REALS});
-    Interval& Mx = cn.create_dom(Interval::ALL_REALS); // x of landmark
-    Interval& My = cn.create_dom(Interval::ALL_REALS); // y of landmark
-    Interval& d1 = cn.create_dom(Interval::ALL_REALS); // x distance of landmark
-    Interval& d2 = cn.create_dom(Interval::ALL_REALS); // y distance of landmark
-    Interval& theta = cn.create_dom(Interval::ALL_REALS); // Bearing of the measurement
-    cn.add(ctc::eval, {obs[0], p, x, v}); // Evaluation of the state vector x at time obs[0]
-    cn.add(ctc_sub, {Mx, p[0], d1}); // d1 = mx - x
-    cn.add(ctc_sub, {My, p[1], d2}); // d2 = my - y
-    cn.add(ctc_add, {p[2], obs[2], theta}); // theta = x3 + y1
-    cn.add(ctc::polar, {d1, d2, obs[1], theta}); // Range and bearing contractor
-    cn.add(ctc_constell, {Mx, My}); // Contract the position of the landmark
-  }
-  // Derivative contractor
-  CtcFunction f(Function("x[4]", "u[2]", "a[4]", "(x[3]*cos(x[2]) - a[0] ; x[3]*sin(x[2]) - a[1] ; u[0] - a[2] ; u[1] - a[3])"));
-  cn.add(ctc::deriv, {x, v});
-  cn.add(f, {x, u, v});
-  
-  //----------------- Graphics
-  vibes::beginDrawing();
+    //Tube
+    TubeVector x(tdomain, dt, 4);
+    x[2] = Tube(actual_x[2], dt).inflate(0.01);
+    x[3] = Tube(actual_x[3], dt).inflate(0.01);
 
-  VIBesFigMap fig_map("Map");
-  fig_map.set_properties(100, 100, 600, 300);
-  fig_map.smooth_tube_drawing(true);
-  fig_map.add_tube(&x, "x", 0, 1);
-  fig_map.add_trajectory(&actual_x, "x*", 0, 1);
-  fig_map.add_observations(v_obs, &actual_x); // drawing obs
-  for(const auto& b : v_map)
-    fig_map.add_beacon(b.mid(), 0.1); // drawing beacons
-  
+    //-------------- Contraction
+    ContractorNetwork cn;
+    TubeVector v(tdomain, dt, 4); // Derivative of x
+    CtcFunction ctc_add(Function("x1", "x2", "x3", "x1 + x2 - x3")); //Add contractor function
+    CtcFunction ctc_sub(Function("x", "y", "z", "x - y - z")); //Sub contractor function
+    CtcConstell ctc_constell(v_map); //Constellation contractor on map v_map
+    for (auto &obs : v_obs) // For each observation
+    {
+        // State vector at the time of the observation
+        IntervalVector &p = cn.create_dom(
+                {Interval::ALL_REALS, Interval::ALL_REALS, Interval::ALL_REALS, Interval::ALL_REALS});
+        Interval &Mx = cn.create_dom(Interval::ALL_REALS); // x of landmark
+        Interval &My = cn.create_dom(Interval::ALL_REALS); // y of landmark
+        Interval &d1 = cn.create_dom(Interval::ALL_REALS); // x distance of landmark
+        Interval &d2 = cn.create_dom(Interval::ALL_REALS); // y distance of landmark
+        Interval &theta = cn.create_dom(Interval::ALL_REALS); // Bearing of the measurement
+        cn.add(ctc::eval, {obs[0], p, x, v}); // Evaluation of the state vector x at time obs[0]
+        cn.add(ctc_sub, {Mx, p[0], d1}); // d1 = mx - x
+        cn.add(ctc_sub, {My, p[1], d2}); // d2 = my - y
+        cn.add(ctc_add, {p[2], obs[2], theta}); // theta = x3 + y1
+        cn.add(ctc::polar, {d1, d2, obs[1], theta}); // Range and bearing contractor
+        cn.add(ctc_constell, {Mx, My}); // Contract the position of the landmark
+    }
+    // Derivative contractor
+    CtcFunction f(Function("x[4]", "u[2]", "a[4]",
+                           "(x[3]*cos(x[2]) - a[0] ; x[3]*sin(x[2]) - a[1] ; u[0] - a[2] ; u[1] - a[3])"));
+    cn.add(ctc::deriv, {x, v});
+    cn.add(f, {x, u, v});
 
-  cn.contract(true);
+    //----------------- Graphics
+    vibes::beginDrawing();
 
-  fig_map.show(0.5);
+    VIBesFigMap fig_map("Map");
+    fig_map.set_properties(100, 100, 600, 300);
+    fig_map.smooth_tube_drawing(true);
+    fig_map.add_tube(&x, "x", 0, 1);
+    fig_map.add_trajectory(&actual_x, "x*", 0, 1);
+    fig_map.add_observations(v_obs, &actual_x); // drawing obs
+    for (const auto &b : v_map)
+        fig_map.add_beacon(b.mid(), 0.1); // drawing beacons
+
+
+    cn.contract(true);
+
+    fig_map.show(0.5);
 }
